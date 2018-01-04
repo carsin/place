4c157752158b400b65d0af3eed90905d0a058496
carsin <carsin@users.noreply.github.com>
2018-01-03 20:52:06 -1000

    basic socket.io set up

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
+app.use(express.static("public"))
+
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(port, console.log("Server listening on port " + port));
+io.on("connection", function(socket){
+    console.log("a user has connected");
+
+    socket.on("disconnect", () => {
+        console.log("a user has disconnected");
+    });
+});
+server.listen(port, console.log("Server listening on port " + port));
