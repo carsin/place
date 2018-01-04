237d7acd22de95af2183677c8445ce47cec58791
carsin <carsin@users.noreply.github.com>
2018-01-04 09:48:21 -1000

    add rudimentary file array loading

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,26 @@
 var express = require("express");
 var router = express.Router();
+var fs = require("fs");
+var clients = {};
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-    res.io.on("connection", function(socket) {
-        console.log("a user has connected");
+    res.io.sockets.on("connection", function(socket) {
+        fs.readFile("db/canvas.txt", (err, data) => {
+            if (err) throw err;
+            var data = data.toString();
+            var items = data.split(/\r?\n/).map( pair => pair.split(/\s+/).map(Number) );
+            console.log(items[1][1]);
+            console.log(items);
+            res.io.emit("send canvas", items)
+        });
 
         socket.on("disconnect", function() {
             console.log("a user has disconnected");
         });
     });
 
-    res.render("index", { title: "Express" });
+    res.render("index");
 });
 
 module.exports = router;
