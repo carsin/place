b4d5d5bcd6681ddb0518eef6bdf6c31fd6e54b96
carsin <carsin@users.noreply.github.com>
2018-01-04 14:20:54 -1000

    general: update

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,6 @@ var clients = {};
 /* GET home page. */
 router.get("/", function(req, res, next) {
     res.io.sockets.on("connection", function(socket) {
-        fs.readFile("db/canvas.txt", (err, data) => {
-            if (err) throw err;
-            var data = data.toString();
-            var items = data.split(/\r?\n/).map( pair => pair.split(/\s+/).map(Number) );
-            console.log(items[1][1]);
-            console.log(items);
-            res.io.emit("send canvas", items)
-        });
 
         socket.on("disconnect", function() {
             console.log("a user has disconnected");
