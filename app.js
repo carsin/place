415b7985e7bc20ab0ef84ebc808c602aa435df7c
carsin <carsin@users.noreply.github.com>
2018-01-05 09:01:54 -1000

    add map loading

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require("serve-favicon");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+var fs = require("fs");
 
 var index = require("./routes/index");
 
@@ -49,6 +50,14 @@ app.use(function(err, req, res, next) {
 
 io.on("connection", function(socket){
     console.log("a user connected");
+    fs.readFile("db/canvas.txt", (err, data) => {
+        if (err) throw err;
+        var data = data.toString();
+        var items = data.split(/\r?\n/).map( pair => pair.split(/\s+/).map(Number) );
+        items.splice(-1,1)
+        console.log(items);
+        io.emit("send canvas", items)
+    });
 
     socket.on("disconnect", () => {
         console.log("a user disconnected");
