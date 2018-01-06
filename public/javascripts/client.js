187482100e57b67c979b9b980fde9e0841e17d5e
carsin <carsin@users.noreply.github.com>
2018-01-05 14:34:37 -1000

    add color switching on click

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,16 +1,32 @@
 var socket = io();
+var listenersSet = false;
 
 socket.on("send canvas", (items) => {
     $("#map").html(" ")
-
     var str;
 
     for (y = 0; y < items.length; y++) {
         str += "<tr>";
         for (x = 0; x < items[y].length; x++) {
-            str += '<td data-color="' + items[y][x] + '">' + items[y][x] + '</td>';
+            str += '<td data-colorid="' + items[y][x] + '"></td>';
         }
         str += "</tr>";
     }
+
     $("#map").append(str);
+    if (!listenersSet) {
+
+    }
+
 });
+
+$(document).click((event) => {
+    let el = $(event.target);
+    let elColor = el.attr("data-colorid");
+    if (elColor >= 4) {
+        el.attr("data-colorid", 0);
+    } else {
+        el.attr("data-colorid", parseInt(elColor) + 1);
+    }
+});
+
